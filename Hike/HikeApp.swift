4b13a29088cabfb046c133fd5b327c48069c970f
Ihor Cherniavskyi <chernyavskiy2019ios@gmail.com>
2023-11-05 18:37:51 +0200

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Ihor Cherniavskyi on 05.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
